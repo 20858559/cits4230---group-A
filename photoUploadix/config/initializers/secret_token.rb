d7dd74f073b10f7e447cb1cf5146b4a5b902516e
Amaud Moret <20858559@ecm-lin-c04.uniwa.uwa.edu.au>
2012-10-11 14:09:05 +0800

    initial ror project

---
+++ photoUploadix/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoUploadix::Application.config.secret_token = 'fad7ff1ae45662fa065723be2ee4a9277671f884bc300ace6d734bab6bc95348cd3e9e75850c7481c17ef9c3ac3699c50266e0dd1c9fefef24602899af9ea73d'
