86706c90761bbeaff6c3b951e717f891f71b4cf2
20858559 <20858955@student.uwa.edu.au>
2012-10-29 17:00:25 +0800

    updated db script

--- photoUploadix/config/cits4230.sql
+++ photoUploadix/config/cits4230.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2012 at 01:03 AM
+-- Generation Time: Oct 29, 2012 at 08:22 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -16,9 +16,36 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+
+DROP TABLE IF EXISTS `albums`;
+DROP TABLE IF EXISTS `photos`;
+DROP TABLE IF EXISTS `albums_photos`;
+DROP TABLE IF EXISTS `users_albums`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `users_photos`;
+DROP TABLE IF EXISTS `users_users`;
+
+
+
+
 --
 -- Database: `cits2430`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(1056) NOT NULL,
+  `is_private` tinyint(1) NOT NULL,
+  `image_cover_location` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -30,28 +57,60 @@ CREATE TABLE `photos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
   `description` varchar(512) NOT NULL,
-  `fileType` varchar(30) DEFAULT NULL,
   `path` varchar(128) NOT NULL,
-  `isPrivate` tinyint(1) NOT NULL,
-  `dateUploaded` datetime DEFAULT NULL,
+  `is_private` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `albumphoto`
+--
+
+CREATE TABLE `albums_photos` (
+  `album_id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES albums(id),
+  FOREIGN KEY (photo_id) REFERENCES photos(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE  `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
-  `userName` varchar(128) NOT NULL,
+  `user_name` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(256) NOT NULL,
   `code` int(8) NOT NULL,
-  PRIMARY KEY (`id`)
+  `registration_token` varchar(256),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `code` (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_album`
+--
+
+CREATE TABLE `users_albums` (
+  `user_id` int(11) NOT NULL,
+  `album_id` int(11) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (album_id) REFERENCES albums(id),
+  PRIMARY KEY (user_id,album_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,13 +118,14 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `userphoto`
 --
 
-CREATE TABLE IF NOT EXISTS `userphoto` (
-  `userId` int(11) NOT NULL,
-  `photoId` int(11) NOT NULL,
-   foreign key(userId) references users(id),
-   foreign key(photoId) references photos(id)
+CREATE TABLE  `users_photos` (
+  `user_id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL,
+  `like` tinyint(1) NOT NULL DEFAULT '0',
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (photo_id) REFERENCES photos(id),
+  PRIMARY KEY (user_id,photo_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 -- --------------------------------------------------------
 
@@ -73,14 +133,15 @@ CREATE TABLE IF NOT EXISTS `userphoto` (
 -- Table structure for table `useruser`
 --
 
-CREATE TABLE IF NOT EXISTS `useruser` (
-  `idOne` int(11) NOT NULL,
-  `idTwo` int(11) NOT NULL,
+CREATE TABLE  `users_users` (
+  `id_one` int(11) NOT NULL,
+  `id_two` int(11) NOT NULL,
   `relationship` int(11) NOT NULL,
-  foreign key(idOne) references users(id),
-  foreign key(idTwo) references users(id)
+  foreign key (id_one) references users(id),
+  foreign key (id_two) references users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
