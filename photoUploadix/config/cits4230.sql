4a5faeb26637763faa43ec4f4a0a45f5ce3d0310
20858559 <20858955@student.uwa.edu.au>
2012-11-11 11:44:47 +0800

    aaa

--- photoUploadix/config/cits4230.sql
+++ photoUploadix/config/cits4230.sql
@@ -1,80 +1,178 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.16, for osx10.6 (i386)
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 29, 2012 at 08:22 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- Host: localhost    Database: cits4230f
+-- ------------------------------------------------------
+-- Server version	5.5.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-DROP DATABASE cits4230f;
-CREATE DATABASE cits4230f;
-USE cits4230f;
 --
--- Database: `cits4230`
---
-
--- --------------------------------------------------------
---
--- Table structure for table `users`
+-- Table structure for table `blocked`
 --
 
-CREATE TABLE  `users` (
+DROP TABLE IF EXISTS `blocked`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blocked` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(128) NOT NULL,
-  `last_name` varchar(128) NOT NULL,
-  `login` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `code` int(8),
-  `registration_token` varchar(256),
-  PRIMARY KEY (`id`)
-  
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `photo_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `photo_id` (`photo_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `blocked_ibfk_1` FOREIGN KEY (`photo_id`) REFERENCES `photos` (`id`),
+  CONSTRAINT `blocked_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `blocked`
+--
 
+LOCK TABLES `blocked` WRITE;
+/*!40000 ALTER TABLE `blocked` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blocked` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `friends`
+--
+
+DROP TABLE IF EXISTS `friends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friends` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_one` int(11) NOT NULL,
+  `id_two` int(11) NOT NULL,
+  `relationship` int(11) NOT NULL,
+  `modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_one` (`id_one`),
+  KEY `id_two` (`id_two`),
+  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`id_one`) REFERENCES `users` (`id`),
+  CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`id_two`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friends`
+--
+
+LOCK TABLES `friends` WRITE;
+/*!40000 ALTER TABLE `friends` DISABLE KEYS */;
+INSERT INTO `friends` VALUES (1,1,2,1,'2012-11-10 20:34:12');
+/*!40000 ALTER TABLE `friends` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `photo_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `modification_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `likes`
+--
+
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+INSERT INTO `likes` VALUES (14,1,1,'2012-11-10 09:32:10');
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `photos`
+--
+
+DROP TABLE IF EXISTS `photos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `photos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `photo_file_name` varchar(128) NOT NULL,
   `description` varchar(512) NOT NULL,
   `is_private` tinyint(1) NOT NULL,
   `photo_content_type` varchar(128) NOT NULL,
-  `photo_file_size` int(11),
-  `photo_updated_at` TIMESTAMP,
-  `user_id` int(11) NULL,
-  `likes` int(11) default 0,
+  `photo_file_size` int(11) DEFAULT NULL,
+  `photo_updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY(`user_id`) REFERENCES users(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-
-
-CREATE TABLE  `friends` (
-  `id_one` int(11) NOT NULL,
-  `id_two` int(11) NOT NULL,
-  `relationship` int(11) NOT NULL,
-  foreign key (id_one) references users(id),
-  foreign key (id_two) references users(id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dummy User....
--- pw foobar
-insert into users (first_name,last_name,login,email,password,code) VALUES ('user_1','a','ewaw','ewa@s.com','3858f62230ac3c915f300c664312c63f','ewqewq');
-insert into users (first_name,last_name,login,email,password,code) VALUES ('user_2','b','ewaw','ewa@s.com','3858f62230ac3c915f300c664312c63f','ewqewq');
+-- Dumping data for table `photos`
+--
 
+LOCK TABLES `photos` WRITE;
+/*!40000 ALTER TABLE `photos` DISABLE KEYS */;
+INSERT INTO `photos` VALUES (1,'home-page.png','Test picture 1',0,'image/png',58915,'2012-11-10 06:09:40',1),(2,'hd.png','test_2',0,'image/png',15901,'2012-11-10 06:58:45',1);
+/*!40000 ALTER TABLE `photos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  `login` varchar(128) DEFAULT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `registration_token` varchar(256) DEFAULT NULL,
+  `last_login_date` date DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Arnaud','Moret','amoret','arnaud.moret@gmail.com','343b1c4a3ea721b2d640fc8700db0f36','',NULL,1),(2,'Arnaud','Moret','user2','arnaud.moret@gmail.com','343b1c4a3ea721b2d640fc8700db0f36','c33b5bd0-0d63-0130-7a56-482a140f6a1e',NULL,0),(3,'Arnaud','Moret','user3','arnaud.moret@gmail.com','343b1c4a3ea721b2d640fc8700db0f36','4f858e40-0d68-0130-7a57-482a140f6a1e',NULL,0),(4,'Arnaud','Moret','user4','arnaud.moret@gmail.com','343b1c4a3ea721b2d640fc8700db0f36','',NULL,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2012-11-11 11:43:55
